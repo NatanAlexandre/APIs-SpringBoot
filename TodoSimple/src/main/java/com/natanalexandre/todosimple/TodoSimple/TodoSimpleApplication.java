31215d00a706b38d48e9db7a68b1154a4680c5e3
Natan Alexandre <natanalexandre742@gmail.com>
2023-10-07 22:55:30 -0300

    Update

---
+++ TodoSimple/src/main/java/com/natanalexandre/todosimple/TodoSimple/TodoSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.natanalexandre.todosimple.TodoSimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSimpleApplication.class, args);
+	}
+
+}
---
+++ TodoSimple/src/test/java/com/natanalexandre/todosimple/TodoSimple/TodoSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natanalexandre.todosimple.TodoSimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
